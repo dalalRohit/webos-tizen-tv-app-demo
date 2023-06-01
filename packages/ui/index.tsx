fe12699d7c2bbd5e25c4e992f6fbb0623f644664
govind saini <govindsa421@gmail.com>
2023-06-01 11:30:28 +0530

    chore: remove import from ui folder

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,7 +1,3 @@
 export * from "./Button";
 export * from "./poc/Poc";
-export * from ".//poc/Pcard";
-export * from "./poc/Plist";
-export * from "./poc/Pmenu";
-export * from "./poc/Prow";
 import "./styles/poc.css";
